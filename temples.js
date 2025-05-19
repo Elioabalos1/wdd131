bb52c5a790164eaaa509bbf8450c62492ee5895b
Elio Abalos <elio.abalos18@gmail.com>
2025-05-19 14:26:20 -0300

    temples

--- temples.js
+++ temples.js
@@ -1,13 +1,11 @@
-/* Current Year */
+
 const currentYear = new Date().getFullYear();
 
-/* Last Mod Date */
 const lastModified = document.lastModified;
 
 document.getElementById("currentyear").textContent = currentYear;
 document.getElementById("lastModified").textContent = "Last Modified: " + lastModified;
 
-/* Hamburger Menu */
 const hamburgerBtn = document.getElementById("hamburgerBtn");
 const mainNav = document.getElementById("mainNav");
 
